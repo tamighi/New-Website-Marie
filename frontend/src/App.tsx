5bfd68a66134c4a6b538be97e097fbdbe7e0983d
thomas <tamighi@hotmail.com>
2022-12-31 14:56:15 +0100

    React router dom basic + fix static assets copy webpack

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,18 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom"
+
 import "./styles.css"
 
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <div>Hello world!</div>,
+  },
+])
+
 export const App = () => {
-  return <div className="App">My app</div>
+  return (
+    <div className="App">
+      <RouterProvider router={router} />
+    </div>
+  )
 }
