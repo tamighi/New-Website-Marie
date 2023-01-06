cdb5455025aa3c1b773af268a7de2eba00dedea3
thomas <tamighi@hotmail.com>
2023-01-06 17:23:16 +0100

    FRONT: Working on background + added assets

--- frontend/src/front/FrontRoot.tsx
+++ frontend/src/front/FrontRoot.tsx
@@ -1,8 +1,10 @@
 import { Outlet } from "react-router-dom"
+import { Background } from "./components/generics/Background"
 
 export const FrontRoot = () => {
   return (
     <div>
+      <Background />
       Front
       <Outlet />
     </div>
---
+++ frontend/src/front/components/generics/Background.tsx
@@ -0,0 +1,15 @@
+export const Background = () => {
+  return (
+    <div
+      style={{
+        width: "100vw",
+        height: "100vh",
+        zIndex: -1,
+        position: "fixed",
+      }}
+    >
+      <img src="./backgrounds/autumn.jpg" alt="background" />
+      Background
+    </div>
+  )
+}
