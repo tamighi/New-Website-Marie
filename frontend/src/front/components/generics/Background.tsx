085973b4462554bdfa144b3a6b20bce456538e09
thomas <tamighi@hotmail.com>
2023-01-10 16:03:38 +0100

    FRONT: Fixed Background

--- frontend/src/front/components/generics/Background.tsx
+++ frontend/src/front/components/generics/Background.tsx
@@ -1,25 +1,9 @@
+import "../../styles/Background.css"
+
 export const Background = () => {
   return (
-    <div
-      style={{
-        width: "100vw",
-        height: "100vh",
-        zIndex: -1,
-        position: "fixed",
-      }}
-    >
-      <img
-        style={{
-          position: "absolute",
-          top: 0,
-          minHeight: "100%",
-          maxHeight: "100%",
-          minWidth: "100%",
-          maxWidth: "100%",
-        }}
-        src="./backgrounds/autumn.jpg"
-        alt="background"
-      />
+    <div className="Background">
+      <img src="./backgrounds/autumn.jpg" alt="background" />
     </div>
   )
 }
