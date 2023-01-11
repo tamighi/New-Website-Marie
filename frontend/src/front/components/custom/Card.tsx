84cabe15f50ef2506368ea28af6cf9522564798e
thomas <tamighi@hotmail.com>
2023-01-11 18:02:09 +0100

    FRONT: Card component + better Appbar style

---
+++ frontend/src/front/components/custom/Card.tsx
@@ -0,0 +1,23 @@
+import React from "react"
+import { useColors } from "../../hooks/useColors"
+
+import "../../styles/Card.css"
+
+interface CardProps {
+  children: React.ReactNode
+  style: React.CSSProperties
+}
+
+const Card = ({ children, style }: CardProps) => {
+  const colors = useColors()
+  return (
+    <div
+      className="Card"
+      style={{ backgroundColor: colors.primaryColor, ...style }}
+    >
+      {children}
+    </div>
+  )
+}
+
+export default Card
--- frontend/src/front/components/generics/appbar/Appbar.tsx
+++ frontend/src/front/components/generics/appbar/Appbar.tsx
@@ -1,8 +1,7 @@
 import { Link } from "react-router-dom"
 
+import Card from "../../custom/Card"
 import { ThemeToggleIcon } from "./ThemeToggleIcon"
-
-import { useColors } from "../../../hooks/useColors"
 
 import "../../../styles/Appbar.css"
 import "../../../styles/colors.css"
@@ -19,26 +18,21 @@ const rightButtons = [
 ]
 
 export const Appbar = () => {
-  const colors = useColors()
-
   return (
-    <div
-      className="Appbar"
-      style={{
-        backgroundColor: colors.primaryColor,
-      }}
-    >
-      <ThemeToggleIcon />
-      <div className="Logo">Marie Somville</div>
-      <ul>
-        {rightButtons.map((button) => (
-          <li key={button.name}>
-            <Link className="Link" to={button.to}>
-              {button.name}
-            </Link>
-          </li>
-        ))}
-      </ul>
-    </div>
+    <Card style={{ marginBottom: "20px" }}>
+      <div className="Appbar">
+        <ThemeToggleIcon />
+        <div className="Logo">Marie Somville</div>
+        <ul>
+          {rightButtons.map((button) => (
+            <li key={button.name}>
+              <Link className="Link" to={button.to}>
+                {button.name}
+              </Link>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </Card>
   )
 }
