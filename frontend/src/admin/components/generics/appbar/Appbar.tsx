5a4748c2a5b57bade77d31e4d8ecded5dcd8e31f
thomas <tamighi@hotmail.com>
2023-01-17 11:44:23 +0100

    FRONT: Use colors in admin

--- frontend/src/admin/components/generics/appbar/Appbar.tsx
+++ frontend/src/admin/components/generics/appbar/Appbar.tsx
@@ -4,9 +4,12 @@ import LibAppbar from "../../../../lib/components/appbar/Appbar"
 import IconButton from "../../../../lib/components/iconButton/IconButton"
 import { ThemeToggleIcon } from "./ThemeToggleIcon"
 
+import useColors from "../../../hooks/useColors"
+
 export const Appbar = () => {
+  const colors = useColors()
   return (
-    <LibAppbar style={{ backgroundColor: "lightblue" }}>
+    <LibAppbar style={{ backgroundColor: colors.primaryColor }}>
       <ThemeToggleIcon />
       <IconButton name="menu" onClick={() => console.log("hello")} />
       Appbar
---
+++ frontend/src/admin/hooks/useColors.tsx
@@ -0,0 +1,20 @@
+import { useDarkTheme } from "./ThemeContext"
+import "../styles/colors.css"
+
+const useColors = () => {
+  const darkTheme = useDarkTheme()
+
+  return darkTheme
+    ? {
+        primaryColor: "var(--dark-color)",
+        secondaryColor: "var(--dark-secondary-color)",
+        textColor: "var(--dark-text-color)",
+      }
+    : {
+        primaryColor: "var(--light-color)",
+        secondaryColor: "var(--light-secondary-color)",
+        textColor: "var(--light-text-color)",
+      }
+}
+
+export default useColors
