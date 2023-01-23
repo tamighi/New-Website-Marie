9789d76397bd75bdd5d8b6ed32669e82e205a6e3
thomas <tamighi@hotmail.com>
2023-01-23 15:03:41 +0100

    Added libraries and access to envir

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,7 +1,11 @@
-import { Module } from '@nestjs/common';
+import { Module } from "@nestjs/common"
+import { ConfigModule } from "@nestjs/config"
+import { getEnvPath } from "./common/utils/envFinder.utils"
+
+const envFilePath: string = getEnvPath(`${__dirname}/common/envs`)
 
 @Module({
-  imports: [],
+  imports: [ConfigModule.forRoot({ envFilePath, isGlobal: true })],
   controllers: [],
   providers: [],
 })
