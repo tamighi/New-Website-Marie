31a41c4db168a919f3e382c6f57200e789b2710c
thomas <tamighi@hotmail.com>
2023-01-23 15:43:35 +0100

    Added env file and database connection

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,11 +1,16 @@
 import { Module } from "@nestjs/common"
 import { ConfigModule } from "@nestjs/config"
+import { TypeOrmModule } from "@nestjs/typeorm"
+import { TypeOrmConfigService } from "./config/typeorm/typeorm.service"
 import { getEnvPath } from "./common/utils/envFinder.utils"
 
 const envFilePath: string = getEnvPath(`${__dirname}/common/envs`)
 
 @Module({
-  imports: [ConfigModule.forRoot({ envFilePath, isGlobal: true })],
+  imports: [
+    ConfigModule.forRoot({ envFilePath, isGlobal: true }),
+    TypeOrmModule.forRootAsync({ useClass: TypeOrmConfigService }),
+  ],
   controllers: [],
   providers: [],
 })
