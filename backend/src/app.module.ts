8cdb1566f7cbc46843220d99cc19e67dfced8ebc
thomas <tamighi@hotmail.com>
2023-01-28 11:49:07 +0100

    Added serviceCategories model in backend

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -3,6 +3,7 @@ import { ConfigModule } from "@nestjs/config"
 import { TypeOrmModule } from "@nestjs/typeorm"
 import { TypeOrmConfigService } from "./config/typeorm/typeorm.service"
 import { getEnvPath } from "./common/utils/envFinder.utils"
+import { ServiceCategoriesModule } from "./models/serviceCategories/serviceCategories.module"
 
 const envFilePath: string = getEnvPath(`${__dirname}/common/envs`)
 
@@ -10,6 +11,7 @@ const envFilePath: string = getEnvPath(`${__dirname}/common/envs`)
   imports: [
     ConfigModule.forRoot({ envFilePath, isGlobal: true }),
     TypeOrmModule.forRootAsync({ useClass: TypeOrmConfigService }),
+    ServiceCategoriesModule,
   ],
   controllers: [],
   providers: [],
