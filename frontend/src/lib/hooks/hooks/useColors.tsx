05b9cd37166e248cd6cfdb904a274a0ec94fca6f
thomas <tamighi@hotmail.com>
2023-01-30 14:27:07 +0100

    Set document properties immediately

--- frontend/src/lib/hooks/hooks/useColors.tsx
+++ frontend/src/lib/hooks/hooks/useColors.tsx
@@ -1,22 +1,28 @@
 import { useDarkTheme } from "../contexts/ThemeContext"
 import React from "react"
 
+const setDocumentProperties = (darkTheme: boolean | null) => {
+  document.documentElement.style.setProperty(
+    "--primary-color",
+    darkTheme ? "var(--dark-primary-color)" : "var(--light-primary-color)"
+  )
+  document.documentElement.style.setProperty(
+    "--secondary-color",
+    darkTheme ? "var(--dark-secondary-color)" : "var(--light-secondary-color)"
+  )
+  document.documentElement.style.setProperty(
+    "--text-color",
+    darkTheme ? "var(--dark-text-color)" : "var(--light-text-color)"
+  )
+}
+
 const useColors = () => {
   const darkTheme = useDarkTheme()
 
+  setDocumentProperties(darkTheme)
+
   React.useEffect(() => {
-    document.documentElement.style.setProperty(
-      "--primary-color",
-      darkTheme ? "var(--dark-primary-color)" : "var(--light-primary-color)"
-    )
-    document.documentElement.style.setProperty(
-      "--secondary-color",
-      darkTheme ? "var(--dark-secondary-color)" : "var(--light-secondary-color)"
-    )
-    document.documentElement.style.setProperty(
-      "--text-color",
-      darkTheme ? "var(--dark-text-color)" : "var(--light-text-color)"
-    )
+    setDocumentProperties(darkTheme)
   }, [darkTheme])
 }
 
