29ff137cbc7cce28b2bf6bec215a29e613ece353
thomas <tamighi@hotmail.com>
2023-02-02 17:42:19 +0100

    Working on structure // added constants

--- frontend/src/lib/hooks/useStyles.tsx
+++ frontend/src/lib/hooks/useStyles.tsx
@@ -1,37 +1,10 @@
 import React from "react"
+
 import useTheme from "./useTheme"
 
-interface Palette {
-  light: string
-  dark: string
-}
+import Theme from "@lib/types/Theme"
 
-interface Theme {
-  palette: {
-    darkMode: boolean
-    primary: Palette
-    secondary: Palette
-    text: Palette
-  }
-}
-
-const baseTheme: Theme = {
-  palette: {
-    darkMode: false,
-    primary: {
-      light: "blue",
-      dark: "grey",
-    },
-    secondary: {
-      light: "white",
-      dark: "lightGrey",
-    },
-    text: {
-      light: "black",
-      dark: "white",
-    },
-  },
-}
+import baseTheme from "@lib/constants/baseTheme.constants"
 
 const useStyles = () => {
   const providedTheme = useTheme()
@@ -39,7 +12,7 @@ const useStyles = () => {
   const theme: Theme = providedTheme || baseTheme
 
   const styles: React.CSSProperties = {
-    backgroundColor: theme.palette.primary.light
+    backgroundColor: theme.palette.primary.light,
   }
 
   return styles
--- frontend/src/lib/providers/ThemeProvider.tsx
+++ frontend/src/lib/providers/ThemeProvider.tsx
@@ -1,73 +1,23 @@
 import React from "react"
 
 import ThemeContext from "@lib/contexts/ThemeContext"
+import ToggleThemeContext from "@lib/contexts/ToggleThemeContext"
 
-type DeepPartial<T> = T extends object
-  ? {
-      [P in keyof T]?: DeepPartial<T[P]>
-    }
-  : T
+import Theme from "@lib/types/Theme"
 
-interface Palette {
-  light: string
-  dark: string
-}
-
-interface Theme {
-  palette: {
-    darkMode: boolean
-    primary: Palette
-    secondary: Palette
-    text: Palette
-  }
-}
-
-const baseTheme: Theme = {
-  palette: {
-    darkMode: false,
-    primary: {
-      light: "blue",
-      dark: "grey",
-    },
-    secondary: {
-      light: "white",
-      dark: "lightGrey",
-    },
-    text: {
-      light: "black",
-      dark: "white",
-    },
-  },
-}
-
-export const createTheme = (createdTheme: DeepPartial<Theme>) => {
-  if (createdTheme.palette?.primary) {
-    Object.assign(baseTheme.palette.primary, createdTheme.palette.primary)
-  }
-  if (createdTheme.palette?.secondary) {
-    Object.assign(baseTheme.palette.secondary, createdTheme.palette.secondary)
-  }
-  if (createdTheme.palette?.text) {
-    Object.assign(baseTheme.palette.text, createdTheme.palette.text)
-  }
-}
-
-
-export const ThemeProvider = ({ children }: { children: React.ReactNode }) => {
-  const [theme, setTheme] = React.useState<Theme>({
-    ...baseTheme,
-    palette: {
-      ...baseTheme.palette,
-      darkMode: localStorage.getItem("dark") === "light" ? false : true,
-    },
-  })
-
+const ThemeProvider = ({
+  children,
+  theme,
+}: {
+  children: React.ReactNode
+  theme: Theme
+}) => {
   const toggleTheme = () => {
     localStorage.setItem("dark", theme ? "light" : "dark")
-    setTheme({
+    theme = {
       ...theme,
       palette: { ...theme.palette, darkMode: !theme.palette?.darkMode },
-    })
+    }
   }
   return (
     <ThemeContext.Provider value={theme}>
@@ -77,3 +27,5 @@ export const ThemeProvider = ({ children }: { children: React.ReactNode }) => {
     </ThemeContext.Provider>
   )
 }
+
+export default ThemeProvider
