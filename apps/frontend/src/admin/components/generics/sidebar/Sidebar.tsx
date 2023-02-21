2d11f27bfd67f72f4d183a7d524cb7f0a86feaef
thomas <tamighi@hotmail.com>
2023-02-21 15:42:03 +0100

    Sidebar ok

--- apps/frontend/src/admin/components/generics/sidebar/Sidebar.tsx
+++ apps/frontend/src/admin/components/generics/sidebar/Sidebar.tsx
@@ -1,11 +1,10 @@
-import React from "react";
-import { Drawer, HomeIcon, TableChartIcon } from "lib";
+import { Divider, Drawer, HomeIcon, TableChartIcon } from "lib";
 import { useNavigate } from "react-router-dom";
 
 import styles from "./Sidebar.css";
 
 const pages = [
-  { name: "Dashboard", to: "/admin", logo: <HomeIcon /> },
+  { name: "Dashboard", to: "/admin", logo: <HomeIcon />, divider: true },
   { name: "Services", to: "/admin/services", logo: <TableChartIcon /> },
 ];
 
@@ -24,28 +23,20 @@ export const Sidebar = ({ open, toggleSideBar }: SidebarProps) => {
     }
   };
 
-  const [visibleTitles, setVisibleTitles] = React.useState(open);
-
-  React.useEffect(() => {
-    const timer = setTimeout(() => {
-      setVisibleTitles(open);
-    }, 500);
-    return () => clearTimeout(timer);
-  }, [open]);
-
   return (
     <>
       <div
         className={styles.Sidebar}
-        style={{ width: `${open ? "150px" : "45px"}` }}
+        style={{ width: `${open ? "200px" : "50px"}` }}
       >
         <ul>
           {pages.map((page, index) => (
             <li key={index}>
               <button onClick={() => navigate(page.to)}>
                 {page.logo}
-                {(visibleTitles? open : visibleTitles) && page.name}
+                {open && page.name}
               </button>
+              {page.divider && <Divider />}
             </li>
           ))}
         </ul>
