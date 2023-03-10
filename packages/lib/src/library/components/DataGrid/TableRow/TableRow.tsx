8c799fc19265cd2ca246338834117df976c00d1b
thomas <tamighi@hotmail.com>
2023-03-10 09:31:57 +0100

    Created simple TableRow

---
+++ packages/lib/src/library/components/DataGrid/TableRow/TableRow.tsx
@@ -0,0 +1,17 @@
+import CSSClasses from "./TableRow.css";
+
+export type TableRowProps = React.HTMLAttributes<HTMLTableRowElement>;
+
+const TableRow = (props: TableRowProps) => {
+  const { children, className, style: customStyle, ...rest } = props;
+
+  const classNames = `${CSSClasses.TableRow} ` + (className || "");
+
+  return (
+    <tr {...rest} style={customStyle} className={classNames}>
+      {children}
+    </tr>
+  );
+};
+
+export default TableRow;
