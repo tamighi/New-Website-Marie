f02af0c248622c97ca67095ff2cd59f994748c30
thomas <tamighi@hotmail.com>
2023-03-24 10:35:08 +0100

    Removed animation for nested routes

--- apps/frontend/src/App.tsx
+++ apps/frontend/src/App.tsx
@@ -17,16 +17,26 @@ import "./Global.css";
 export const App = () => {
   const location = useLocation();
 
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  const [_, route] = location.pathname.split("/");
+
   return (
     <div className={styles.App}>
       <Appbar />
       <Background />
       <AnimatePresence mode="wait">
-        <Routes location={location} key={location.pathname}>
+        <Routes location={location} key={route}>
           <Route path="" element={<HomePage />} />
           <Route path="contact" element={<ContactPage />} />
-          <Route path="services" element={<ServicePage />} />
-          <Route path="services/:id" element={<ServicePage />} />
+          <Route
+            path="services/*"
+            element={
+              <Routes>
+                <Route path="" element={<ServicePage />} />
+                <Route path=":id" element={<ServicePage />} />
+              </Routes>
+            }
+          />
           <Route path="livredor" element={<GoldenBookPage />} />
           <Route path="avis" element={<ReviewPage />} />
           <Route path="*" element={<ErrorPage />} />
