ec5693820249bba902e789105a2fccb591adae8d
thomas <tamighi@hotmail.com>
2023-04-06 19:09:12 +0200

    style(frontend): Page display more centered

--- apps/frontend/src/components/generics/basePage/CenteredPage.tsx
+++ apps/frontend/src/components/generics/basePage/CenteredPage.tsx
@@ -3,13 +3,18 @@ import React from "react";
 import { Paper, ResponsiveGrid as Grid } from "lib";
 import { BasePage } from "./BasePage";
 
+// TODO: Better display styles
 export const CenteredPage = ({ children }: { children: React.ReactNode }) => {
   return (
     <BasePage>
       <Grid container>
         <Grid large={3} />
-        <Grid small={12} large={6} style={{ width: "100%" }}>
-          <Paper style={{ padding: "6px", width: "100%" }}>{children}</Paper>
+        <Grid small={12} large={6} style={{ width: "100%", display: "inline-flex" }}>
+          <Paper
+            style={{ padding: "6px", width: "100%" }}
+          >
+            {children}
+          </Paper>
         </Grid>
         <Grid large={3} />
       </Grid>
