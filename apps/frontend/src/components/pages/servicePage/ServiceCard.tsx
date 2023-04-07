8b86393224da8958c80f999885d8d93aef50cc3a
thomas <tamighi@hotmail.com>
2023-04-07 08:11:41 +0200

    style(frontend): Secondary color on servicepage

--- apps/frontend/src/components/pages/servicePage/ServiceCard.tsx
+++ apps/frontend/src/components/pages/servicePage/ServiceCard.tsx
@@ -1,17 +1,22 @@
-import { Card } from "lib";
+import { Card, useStyles } from "lib";
 import { useNavigate } from "react-router-dom";
 import { ServiceDto } from "resources/service";
 
 import styles from "./Service.css";
 
+//TODO: lib: Accessing color more easily (useGetColor("type"))
 export const ServiceCard = ({ service }: { service: ServiceDto }) => {
   const navigate = useNavigate();
+  const themeStyle = useStyles({ type: "secondary" });
 
   return (
     <Card
       className={styles.ServiceCard}
       onClick={() => navigate(`${service.id}`)}
-      style={{ transition: "transform .2s" }}
+      style={{
+        transition: "transform .2s",
+        border: `3px solid ${themeStyle.backgroundColor}`,
+      }}
     >
       <h3>{service.name}</h3>
       <p className={styles.DescriptionClamp}>{service.description}</p>
