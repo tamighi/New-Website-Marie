982566e2cd3e3ff3b6524a70ac2884ee1fde936c
thomas <tamighi@hotmail.com>
2023-04-08 16:18:27 +0200

    feat(lib): Grid has now more breakpoints

--- packages/lib/src/dev/components/TestPage.tsx
+++ packages/lib/src/dev/components/TestPage.tsx
@@ -1,4 +1,4 @@
-import { Button, Card } from "library";
+import { Button, Card, Grid } from "library";
 import styles from "../styles/Page.css";
 
 export const TestPage = () => {
@@ -10,11 +10,17 @@ export const TestPage = () => {
           alignItems: "center",
           justifyContent: "center",
           width: "500px",
-          height: "500px"
+          height: "500px",
         }}
       >
-        <Button variant="contained">Contained</Button>
-        <Button variant="text">Outlined</Button>
+        <Grid container>
+          <Grid xs={12} sm={8} md={6} lg={3}>
+            <Button variant="contained">Contained</Button>
+          </Grid>
+          <Grid xs={12} sm={8} md={6} lg={3}>
+            <Button variant="text">Outlined</Button>
+          </Grid>
+        </Grid>
       </Card>
     </div>
   );
--- packages/lib/src/library/components/Grid/Grid.tsx
+++ packages/lib/src/library/components/Grid/Grid.tsx
@@ -1,31 +1,66 @@
+import React from "react";
 import CSSClasses from "./Grid.css";
 
-export interface GridProps extends React.HTMLAttributes<HTMLDivElement> {
+type BreakPoints = "xs" | "sm" | "md" | "lg" | "xl";
+
+const getCurrentBreakPoint = (): BreakPoints => {
+  const windowWidth = window.innerWidth;
+  if (windowWidth >= 1536) {
+    return "xl";
+  } else if (windowWidth >= 1200) {
+    return "lg";
+  } else if (windowWidth >= 900) {
+    return "md";
+  } else if (windowWidth >= 600) {
+    return "sm";
+  } else {
+    return "xs";
+  }
+};
+
+export type GridProps = React.HTMLAttributes<HTMLDivElement> & {
   container?: boolean;
-  small?: number;
-  large?: number;
-}
+} & { [K in BreakPoints]?: number };
 
 const Grid = (props: GridProps) => {
-  const { style, children, className, container, small, large, ...rest } =
-    props;
+  const {
+    style = {},
+    children,
+    className = "",
+    container,
+    xs = 1,
+    sm = xs,
+    md = sm,
+    lg = md,
+    xl = lg,
+    ...rest
+  } = props;
 
-  const classNames = [
-    container ? CSSClasses.GridContainer : CSSClasses.GridItem,
-  ];
+  const breakPoints = { xs, sm, md, lg, xl };
 
-  if (!container) {
-    classNames.push(CSSClasses[`small-${small}`]);
-    classNames.push(CSSClasses[`large-${large}`]);
-  }
+  const [currentBreakPoint, setCurrentBreakPoint] =
+    React.useState<BreakPoints>(getCurrentBreakPoint);
 
-  if (className) {
-    classNames.push(className);
-  }
+  const classNames =
+    `${container ? CSSClasses.GridContainer : ""} ` + className;
+
+  React.useEffect(() => {
+    function handleResize() {
+      setCurrentBreakPoint(getCurrentBreakPoint());
+    }
+    window.addEventListener("resize", handleResize);
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
+
+  style.gridColumn =
+    style.gridColumn ||
+    (container ? "" : `span ${breakPoints[currentBreakPoint]}`);
 
   return (
-    <div className={classNames.join(" ")} style={style} {...rest}>
-      {children || ""}
+    <div className={classNames} style={style} {...rest}>
+      {children}
     </div>
   );
 };
