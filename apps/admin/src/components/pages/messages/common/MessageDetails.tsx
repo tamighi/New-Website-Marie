f9ed2e3eb61db65b426b94d2acc66bd9dfdcc404
thomas <tamighi@hotmail.com>
2023-04-09 12:16:15 +0200

    ci(lib): Small modifications datagrid clickable

--- apps/admin/src/components/pages/messages/common/MessageDetails.tsx
+++ apps/admin/src/components/pages/messages/common/MessageDetails.tsx
@@ -16,9 +16,15 @@ export const MessageDetails = <T extends ResourceType<MessageResourceString>>(
     <p>
       Email: {message.email}
       <br />
+      <br />
       Nom : {message.name}
       <br />
+      <br />
+      Status : {message.status}
+      <br />
+      <br />
       Message : <br />
+      <br />
       {message.message}
       <br />
     </p>
