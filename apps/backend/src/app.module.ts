ce397e4446ba61baf1e2596bfcfc54eebe97ee18
thomas <tamighi@hotmail.com>
2023-04-18 17:57:51 +0200

    ci(global): Prod environment setup

--- apps/backend/src/app.module.ts
+++ apps/backend/src/app.module.ts
@@ -1,6 +1,8 @@
 import { Module } from "@nestjs/common";
 import { TypeOrmModule } from "@nestjs/typeorm";
 import { TypeOrmConfigService } from "./config/typeorm/typeorm.service";
+import { ServeStaticModule } from "@nestjs/serve-static";
+import { join } from "path";
 
 import { ServicesModule } from "./models/service/services.module";
 import { AuthModule } from "./auth/auth.module";
@@ -10,9 +12,15 @@ import { QuestionsModule } from "./models/question/questions.module";
 import { DevisModule } from "./models/devis/devis.module";
 import { ReviewsModule } from "./models/review/reviews.module";
 
-
 @Module({
   imports: [
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname, "../..", "admin", "build"),
+      serveRoot: "/admin"
+    }),
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname, "../..", "frontend", "build"),
+    }),
     TypeOrmModule.forRootAsync({ useClass: TypeOrmConfigService }),
     AuthModule,
     UsersModule,
