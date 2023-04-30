024dc0afb58edcff2e34e1462740f85a9ac1aec3
thomas <tamighi@hotmail.com>
2023-04-30 16:19:58 +0200

    chore(admin): Add devis variables

--- apps/admin/src/api/types/devis.ts
+++ apps/admin/src/api/types/devis.ts
@@ -1,5 +1,12 @@
 import { messageDto, MessageDto } from "./message";
+import { ServiceDto, SubServiceDto } from "./service";
 
-export type DevisDto = MessageDto;
+export type DevisDto = MessageDto & {
+  nbCharacter: number;
+  price?: number;
+  endDate?: Date;
+  subService?: SubServiceDto;
+  service?: ServiceDto;
+};
 
-export const devisDto: DevisDto = { ...messageDto };
+export const devisDto: DevisDto = { ...messageDto, nbCharacter: 1 };
