31e392dfbc1365994c9f715f82caaa069255b5ae
tamighi <thomas.amighi@hotmail.com>
2023-06-15 09:39:16 +0200

    ci(admin): Add delete button for messages

--- apps/admin/src/pages/messages/common/MessageDetails.tsx
+++ apps/admin/src/pages/messages/common/MessageDetails.tsx
@@ -1,7 +1,8 @@
 import { ResourceType } from "types";
 import { SimpleField } from "components";
-import { Grid } from "lib";
+import { Button, Grid } from "lib";
 import { StatusField } from "./StatusField";
+import { useDeleteOne, useGetCurrentQuery } from "hooks";
 
 type MessageResourceString = "question" | "review" | "devis";
 
@@ -14,6 +15,9 @@ const MessageDetails = <T extends ResourceType<MessageResourceString>>(
   props: MessageDetailsProps<T>
 ) => {
   const { message, resource } = props;
+
+  const query = useGetCurrentQuery();
+  const { mutate } = useDeleteOne(resource, query);
 
   return (
     <Grid container style={{ gap: "16px" }}>
@@ -33,6 +37,20 @@ const MessageDetails = <T extends ResourceType<MessageResourceString>>(
       </Grid>
       <Grid xs={12}>
         <SimpleField label="Message">{message.message}</SimpleField>
+      </Grid>
+      <Grid xs={12}>
+        <Button
+          style={{
+            color: "red",
+            border: "2px solid red",
+            borderRadius: "10px",
+            fontWeight: "700",
+            fontSize: "18px",
+          }}
+          onClick={() => mutate({ id: message.id })}
+        >
+          Delete
+        </Button>
       </Grid>
     </Grid>
   );
