1b72b429c8bb310cd61ebec0aee32fdafd12277e
thomas <tamighi@hotmail.com>
2023-06-21 16:43:44 +0200

    ci(admin): Basic not found page

--- apps/admin/src/pages/ErrorPage/ErrorPage.tsx
+++ apps/admin/src/pages/ErrorPage/ErrorPage.tsx
@@ -1,6 +1,18 @@
 // TODO
 const ErrorPage = () => {
-  return <div>ErrorPage</div>;
+  return (
+    <div
+      style={{
+        height: "100%",
+        width: "100%",
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+      }}
+    >
+      <span>Not found</span>
+    </div>
+  );
 };
 
 export default ErrorPage;
