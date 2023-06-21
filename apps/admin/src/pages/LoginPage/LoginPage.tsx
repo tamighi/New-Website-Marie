d61ca0a3f69425f5d95eebbcd180e99127023069
thomas <tamighi@hotmail.com>
2023-06-21 16:55:05 +0200

    ci(admin): Small fixes

--- apps/admin/src/pages/LoginPage/LoginPage.tsx
+++ apps/admin/src/pages/LoginPage/LoginPage.tsx
@@ -1,17 +1,21 @@
+import React from "react";
+
 import { ICredentials } from "services/auth";
 import { useAuth } from "hooks/useAuth";
-import { Card, Input, useForm } from "lib";
+import { Button, Card, Input, useForm } from "lib";
 
 import styles from "./LoginPage.css";
 
 const LoginPage = () => {
   const { handleSubmit, register, reset } = useForm<ICredentials>();
   const { login } = useAuth();
+  const [error, setError] = React.useState(false);
 
   const onSubmit = async (credentials: Partial<ICredentials>) => {
     try {
       await login(credentials as ICredentials);
     } catch {
+      setError(true);
       reset();
     }
   };
@@ -21,9 +25,13 @@ const LoginPage = () => {
       <Card>
         <h3>Login</h3>
         <form onSubmit={handleSubmit(onSubmit)}>
-          <Input {...register("username")} placeholder="Identifier" />
-          <Input {...register("password")} placeholder="Password" />
-          <input type="submit" />
+          <Input {...register("username")} placeholder="Identifiant" />
+          <Input
+            {...register("password")}
+            placeholder="Mot de passe"
+          />
+          {error && <p>Identifiant ou mot de passe incorrect</p>}
+          <Button type="submit">Submit</Button>
         </form>
       </Card>
     </div>
