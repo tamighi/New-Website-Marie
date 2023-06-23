a9100d0992b375cea88fad68eee484b75ca33edb
thomas <tamighi@hotmail.com>
2023-06-23 12:23:18 +0200

    ci(lib): Deleted dev files and script library

--- packages/lib/webpack.js
+++ packages/lib/webpack.js
@@ -1,9 +1,21 @@
+const path = require("path");
+
 module.exports = {
   entry: "./src/library/index.ts",
   mode: "production",
+  resolve: {
+    extensions: [".tsx", ".ts", ".js"],
+  },
   externals: {
     react: "react",
     "react-dom": "react-dom",
+  },
+  output: {
+    path: path.resolve(__dirname, "build"),
+    filename: "bundle.js",
+    library: {
+      type: "umd",
+    },
   },
   module: {
     rules: [
@@ -22,6 +34,10 @@ module.exports = {
           },
         ],
       },
+      {
+        test: /\.tsx?$/,
+        use: "ts-loader",
+      },
     ],
   },
-}
+};
