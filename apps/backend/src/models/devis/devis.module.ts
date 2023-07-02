b42baafc71d86cd3d00832a8b4d400549cb137b6
thomas <tamighi@hotmail.com>
2023-07-02 17:26:55 +0200

    feat(backend): Add file controller

--- apps/backend/src/models/devis/devis.module.ts
+++ apps/backend/src/models/devis/devis.module.ts
@@ -1,14 +1,14 @@
 import { Module } from "@nestjs/common";
 import { TypeOrmModule } from "@nestjs/typeorm";
+import { MulterModule } from "@nestjs/platform-express";
 
 import { FileModule } from "../core";
+import { MulterConfigService } from "src/config";
 
 import { DevisService } from "./devis.service";
 import { Devis } from "./entities/devis.entity";
 import { DevisController } from "./devis.controller";
 import { MessagesService } from "../message/messages.service";
-import { MulterConfigService } from "src/config";
-import { MulterModule } from "@nestjs/platform-express";
 
 @Module({
   imports: [
