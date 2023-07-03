bc32353c06ac2d6e46440c56b0cd2fdee5d62786
thomas <tamighi@hotmail.com>
2023-07-03 16:33:45 +0200

    fix(frontend): Frontend now up to date with lib

--- apps/frontend/src/contexts/ThemeContext.tsx
+++ apps/frontend/src/contexts/ThemeContext.tsx
@@ -22,17 +22,20 @@ const MyThemeProvider = ({ children }: { children: React.ReactNode }) => {
   const theme = createTheme({
     palette: {
       mode,
-      light: {
-        primary: "rgba(248, 195, 115, 0.8)",
-        secondary: "rgba(165, 30, 30, 0.8)",
-        surface: "rgba(229, 103, 20, 0.8)",
-        text: "black",
-      },
-      dark: {
-        primary: "rgba(60, 80, 200, 0.8)",
-        secondary: "rgba(153, 30, 170, 0.8)",
-        surface: "rgba(0, 0, 96, 0.8)",
-        text: "white",
+      colors: {
+        ...(mode === "light"
+          ? {
+              primary: "rgba(248, 195, 115, 0.8)",
+              secondary: "rgba(165, 30, 30, 0.8)",
+              surface: "rgba(229, 103, 20, 0.8)",
+              text: "black",
+            }
+          : {
+              primary: "rgba(60, 80, 200, 0.8)",
+              secondary: "rgba(153, 30, 170, 0.8)",
+              surface: "rgba(0, 0, 96, 0.8)",
+              text: "white",
+            }),
       },
     },
     transition: "color 0.6s, background 0.6s",
